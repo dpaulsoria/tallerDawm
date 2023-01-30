3fd4b8439d6c9fa0926a2df1c0dc5818681e1d53
Paul Soria <dsoria@espol.edu.ec>
2023-01-30 03:22:49 -0500

    Delete

--- DAWM-apps-admin/album/admin/routes/index.js
+++ DAWM-apps-admin/album/admin/routes/index.js
@@ -11,6 +11,70 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+router.get("/photos/delete/:id", async function (req, res, next) {
+  const URL = "http://localhost:4444/rest/fotos/delete/" + req.params.id;
+  const config = {
+    proxy: {
+      host: "localhost",
+      port: 4444,
+    },
+  };
+
+  const response = await axios.delete(URL, config);
+
+  if (response.status == "200" && response.statusText == "OK") {
+    res.redirect("/photos");
+  } else {
+    res.redirect("/");
+  }
+});
+
+// router.delete("/photos/delete/:id", async function (req, res, next) {
+//   const URL = "http://localhost:4444/rest/fotos/delete/" + req.params.id;
+//   const config = {
+//     proxy: {
+//       host: "localhost",
+//       port: 4444,
+//     },
+//   };
+
+//   const response = await axios.delete(URL, config);
+
+//   if (response.status == "200" && response.statusText == "OK") {
+//     res.redirect("/photos");
+//   } else {
+//     res.redirect("/");
+//   }
+// });
+
+router.put("/photos/update/:id", async function (req, res, next) {
+  let { title, description, rate } = req.body;
+
+  const URL = "http://localhost:4444/rest/fotos/update/" + req.params.id;
+
+  let data = {
+    titulo: title,
+    descripcion: description,
+    calificacion: rate,
+    ruta: "",
+  };
+
+  const config = {
+    proxy: {
+      host: "localhost",
+      port: 4444,
+    },
+  };
+
+  const response = await axios.put(URL, data, config);
+
+  if (response.status == "200" && response.statusText == "OK") {
+    res.redirect("/photos");
+  } else {
+    res.redirect("/");
+  }
+});
+
 router.get("/photos", async function (req, res, next) {
   const URL = "http://localhost:4444/fotos/findAll/json";
   const config = {
